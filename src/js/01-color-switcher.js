a8f90c2eb334a3cc067ff7ef83a42625d7f0d11a
NadiaStyslo <sir.ihor@gmail.com>
2023-11-29 13:17:27 -0500

    finished1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,26 @@
+const bodyEl = document.querySelector('body')
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(bodyEl.dataset)
 
-startBtn = document.querySelectorData('.start');
-stopBtn = document.querySelectorData('.stop')
+setTimeout
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+//let timeId = null;
+function onClickStart() {
+    startBtn.setAttribute('disabled',"")
+    stopBtn.removeAttribute('disabled')
+    timeId = setInterval(() => {
+        //const bodyColor = getRandomHexColor()
+        bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+//console.log(startBtn)
+startBtn.addEventListener('click', onClickStart);
+function onClickStop() {
+    clearInterval(timeId);
+    //stopBtn.setAttribute('disabled', '')
+    startBtn.removeAttribute('disabled')
+}
+stopBtn.addEventListener('click', onClickStop)

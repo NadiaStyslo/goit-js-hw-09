453ee97c46d3b623119033ad4b29b169087999ff
NadiaStyslo <106289884+NadiaStyslo@users.noreply.github.com>
2023-11-29 18:17:49 +0000

    Deploying to gh-pages from @ NadiaStyslo/goit-js-hw-09@a8f90c2eb334a3cc067ff7ef83a42625d7f0d11a 🚀

---
+++ 01-color-switcher.cf1acdeb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");console.log(t.dataset),setTimeout,e.addEventListener("click",(function(){e.setAttribute("disabled",""),o.removeAttribute("disabled"),timeId=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),o.addEventListener("click",(function(){clearInterval(timeId),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.cf1acdeb.js.map

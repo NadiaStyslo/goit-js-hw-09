38a2a7d77ca83d4b9c1ce62c3bdee2cf75cdafab
NadiaStyslo <106289884+NadiaStyslo@users.noreply.github.com>
2023-12-02 17:55:21 +0000

    Deploying to gh-pages from @ NadiaStyslo/goit-js-hw-09@933cbecf98d69c047e9059ed0d801a7794b924ee 🚀

--- 01-color-switcher.db79baa3.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");console.log(t.dataset),setTimeout,e.addEventListener("click",(function(){e.setAttribute("disabled",""),o.removeAttribute("disabled"),timeId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),o.addEventListener("click",(function(){clearInterval(timeId),e.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.db79baa3.js.map

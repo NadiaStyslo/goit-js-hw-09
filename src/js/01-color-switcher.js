933cbecf98d69c047e9059ed0d801a7794b924ee
NadiaStyslo <sir.ihor@gmail.com>
2023-12-02 12:54:55 -0500

    chenged mistace

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,30 @@
-const bodyEl = document.querySelector('body')
+const bodyEl = document.querySelector('body');
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
-console.log(bodyEl.dataset)
+console.log(bodyEl.dataset);
 
-setTimeout
+setTimeout;
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
-//let timeId = null;
+let timeId = null;
+//stopBtn.setAttribute('disabled', '')
+
 function onClickStart() {
-    startBtn.setAttribute('disabled',"")
-    stopBtn.removeAttribute('disabled')
-    timeId = setInterval(() => {
-        //const bodyColor = getRandomHexColor()
-        bodyEl.style.backgroundColor = getRandomHexColor()
-    }, 1000)
+  startBtn.toggleAttribute('disabled');
+  stopBtn.removeAttribute('disabled');
+  timeId = setInterval(() => {
+    //const bodyColor = getRandomHexColor()
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 //console.log(startBtn)
 startBtn.addEventListener('click', onClickStart);
 function onClickStop() {
-    clearInterval(timeId);
-    //stopBtn.setAttribute('disabled', '')
-    startBtn.removeAttribute('disabled')
+  clearInterval(timeId);
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
 }
-stopBtn.addEventListener('click', onClickStop)
+stopBtn.addEventListener('click', onClickStop);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,61 +1,65 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const buttonEl = document.querySelector('button')
-const formEl = document.querySelector('form')
+const buttonEl = document.querySelector('button');
+const formEl = document.querySelector('form');
 //console.log(buttonEl)
 formEl.style.display = 'flex';
 formEl.style.fontSize = '30px';
-formEl.style.flexDirection = 'column'
+formEl.style.flexDirection = 'column';
 buttonEl.style.fontSize = '30px';
 buttonEl.style.margin = '6px';
-formEl.style.padding = '5px'
-//buttonEl.addEventListener('sumbit', onSumbit);
-formEl.addEventListener('click', onForm)
+formEl.style.padding = '5px';
+//formEl.addEventListener('sumbit', onSumbit);
+formEl.addEventListener('click', onForm);
 
+//function onSumbit() {
+
+//}
 
 //function onSumbit() { }
 function onForm(evt) {
   evt.preventDefault();
-  const {delay, step, amount } = evt.currentTarget.elements;
+  const { delay, step, amount } = evt.currentTarget.elements;
   let delayVal = Number(delay.value);
   let stepVal = Number(step.value);
   let amoutVal = Number(amount.value);
 
-
-let position = 1
+  let position = 1;
   for (let i = 1; i <= amoutVal; i += 1) {
+    // buttonEl.setAttribute('disabled', '')
+
     createPromise(position, delayVal)
-      .then(result =>
-        Notify.success(result)
-      )
-      
-      .catch(error =>
-        Notify.failure(error))
+      // buttonEl.setAttribute('disabled', '')
+      .then(result => Notify.success(result))
+
+      .catch(error => Notify.failure(error));
     position += 1;
-    delayVal = delayVal + stepVal
-  
+    delayVal = delayVal + stepVal;
   }
-  }
+}
 function createPromise(position, delay) {
+  buttonEl.setAttribute('disabled', '');
+
   return new Promise((resolve, reject) => {
-     const shouldResolve = Math.random() > 0.3;
+    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-       resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        }
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      
-    }, delay) 
-    })
-    
-    
-  }
- 
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+   
+  });
+}
 
 // createPromise(2, 1500)
+
+//   //formEl.removeAttribute('disabled')
 //   .then(({ position, delay }) => {
+
 //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
 //   })
 //   .catch(({ position, delay }) => {
+
 //     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
 //   });
